0443cf987bcfd4e871fd992ea61b92035fe74352
meirajh <jhalvesmeira@gmail.com>
2020-09-02 11:38:52 -0300

    requisição get de contratos funcionando

--- webropay-frontend/src/actions/contracts.js
+++ webropay-frontend/src/actions/contracts.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getContracts = () => async (dispatch) => {
   const response = await axios.get(
-    `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/joao-meira/matches`
+    `http://localhost:3000/contracts`
   );
   dispatch(setContracts(response));
   console.log("action get contracts",response);
--- webropay-frontend/src/pages/ContractsPage/components/ContractsList/index.js
+++ webropay-frontend/src/pages/ContractsPage/components/ContractsList/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import {
   ContractsListWrapper,
   ListHeader,
@@ -8,56 +8,21 @@ import {
   ListTitle,
 } from "./style";
 import editIcon from "../../../../img/lapis.svg";
+import axios from "axios";
 
 const ContractsList = (props) => {
-  const items = [
-    {
-      id: "CT-0000013",
-      description: "Contrato de aquisição",
-      status: "fechado",
-    },
-    {
-      id: "CT-0000023",
-      description: "Contrato dos painéis zima blue",
-      status: "aberto",
-    },
-    {
-      id: "CT-00000145",
-      description: "Contrato da doideira doida",
-      status: "fechado",
-    },
-    {
-      id: "CT-000001356",
-      description: "Contrato do zé das couves",
-      status: "fechado",
-    },
-    {
-      id: "CT-000001356",
-      description: "Contrato do zé das couves",
-      status: "fechado",
-    },
-    {
-      id: "CT-000001356",
-      description: "Contrato do zé das couves",
-      status: "fechado",
-    },
-    {
-      id: "CT-000001356",
-      description:
-        "Contrato do zé das couves asdiufh uaheu  aeuhuae huahe uahe uae huae haeuhadsuifhusaih asdifaisud f9io9saud fiouad sifuasdiofu sadiofu asodif ",
-      status: "fechado",
-    },
-  ];
-
   const fields = ["Contrato", "Descrição", "Status"];
 
-    const [currentUser, setCurrentUser] = useState()
+  const [currentContracts, setCurrentContracts] = useState();
 
   useEffect(() => {
-
-  }, [])
-
-  
+    async function getContracts() {
+      const response = await axios.get(`http://localhost:3000/contracts`);
+      setCurrentContracts(response.data);
+      console.log("get contracts", response.data);
+    }
+    getContracts();
+  }, []);
 
   const listTitledRender = fields.map((field, index) => (
     <ListField details={field} key={index}>
@@ -65,29 +30,33 @@ const ContractsList = (props) => {
     </ListField>
   ));
 
-  const listTextRender = items.map((item, itemIndex) => (
-    <ListItem key={itemIndex}>
-      <ListField>
-        <span> {item.id} </span>
-      </ListField>
-      <ListField>
-        <span> {item.description} </span>
-      </ListField>
-      <ListField>
-        <span> {item.status} </span>
-      </ListField>
-      <ListField>
-        <img src={editIcon} />
-      </ListField>
-    </ListItem>
-  ));
+  const listTextRender = currentContracts ? (
+    currentContracts.map((item, itemIndex) => (
+      <ListItem key={itemIndex}>
+        <ListField>
+          <span> {item.id} </span>
+        </ListField>
+        <ListField>
+          <span> {item.description} </span>
+        </ListField>
+        <ListField>
+          <span> {item.status} </span>
+        </ListField>
+        <ListField>
+          <img src={editIcon} />
+        </ListField>
+      </ListItem>
+    ))
+  ) : (
+    <span>Carregando...</span>
+  );
 
   return (
     <ContractsListWrapper>
       <ListHeader>
         {listTitledRender}
-        <ListField >
-          <ListEdit>  </ListEdit>
+        <ListField>
+          <ListEdit> </ListEdit>
         </ListField>
       </ListHeader>
       {listTextRender}
